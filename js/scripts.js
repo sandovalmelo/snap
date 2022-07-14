cc709be66be72eafff75c02647b7ce7bdb777734
Sandoval Melo <juniormelo777@gmail.com>
2022-07-14 16:13:26 -0300

    add large screens media queries on menu section

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 const menu = document.querySelector("menu");
 const hamburguerMenu = document.querySelector(".hamburguer-menu");
 const closeMenu = document.querySelector(".close-menu");
+const main = document.querySelector("main");
 
 hamburguerMenu.addEventListener("click", (event) => {
 	menu.setAttribute("data-open", "true");
+	// document.body.style.overflow = "hidden";
 });
 
 closeMenu.addEventListener("click", (event) => {
 	menu.setAttribute("data-open", "false");
+	// document.body.style.overflow = "auto";
 });
