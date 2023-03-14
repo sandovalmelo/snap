848be9b08d223137405b242f9108e05d921bc731
Sandoval Melo <juniormelo777@gmail.com>
2023-03-14 13:31:50 -0300

    fix dropdown scritps on large and small screens

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ const features = document.querySelector(".features");
 const featuresList = document.querySelector(".features-list");
 const company = document.querySelector(".company");
 const companyList = document.querySelector(".company-list");
+const largeScreens = 1280;
 
 hamburguerMenu.addEventListener("click", (event) => {
 	menu.setAttribute("data-open", "true");
@@ -19,10 +20,40 @@ closeMenu.addEventListener("click", (event) => {
 	main.style.overflow = "asuto";
 });
 
+features.addEventListener("mouseover", (event) => {
+	if (window.innerWidth > largeScreens) {
+		featuresList.classList.add("active");
+		companyList.classList.remove("active");
+	}
+});
+
+featuresList.addEventListener("mouseout", (event) => {
+	if (window.innerWidth > largeScreens) {
+		featuresList.classList.remove("active");
+	}
+});
+
 features.addEventListener("click", (event) => {
-	featuresList.classList.toggle("active");
+	if (window.innerWidth <= largeScreens) {
+		featuresList.classList.toggle("active");
+	}
+});
+
+company.addEventListener("mouseover", (event) => {
+	if (window.innerWidth > largeScreens) {
+		companyList.classList.add("active");
+		featuresList.classList.remove("active");
+	}
+});
+
+companyList.addEventListener("mouseout", (event) => {
+	if (window.innerWidth > largeScreens) {
+		companyList.classList.remove("active");
+	}
 });
 
 company.addEventListener("click", (event) => {
-	companyList.classList.toggle("active");
+	if (window.innerWidth <= largeScreens) {
+		companyList.classList.toggle("active");
+	}
 });
